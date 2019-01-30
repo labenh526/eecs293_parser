799dcdd63540060b0b85af9029ec4d769606781b
NickKettler <nickakettler@gmail.com>
2019-01-30 17:11:30 -0500

    Implemented features of the Variable class. Testing to see if previously created variables exist with the Cache is not yet functional

--- src/Variable.java
+++ src/Variable.java
@@ -1,23 +1,29 @@
-public final class Variable extends AbstractToken implements  Token{
+public final class Variable extends AbstractToken implements Token{
 
-    private String representation = "";
+    //Stores the representation for the variable
+    private String representation = null;
 
-    public Variable(String representation) {
-        this.representation = representation;
-    }
+    //Stores cache of all created variables
+    static Cache<String, Variable> cache;
 
-    @Override
-    public TerminalSignal getType() {
-        return null;
-    }
-
-    @Override
-    public String toString() {
-        return "f";
+    // private Constructor to create variable
+    private Variable(String representation) {
+        this.setRepresentation(representation);
     }
 
     public final String getRepresentation() {
         return representation;
+    }
+
+    //All variables will always return the type VARIABLE
+    @Override
+    public TerminalSignal getType() {
+        return TerminalSignal.VARIABLE;
+    }
+
+    @Override
+    public String toString() {
+        return this.getRepresentation();
     }
 
     public final void setRepresentation(String representation) {
@@ -25,6 +31,16 @@ public final class Variable extends AbstractToken implements  Token{
     }
 
     public static final Variable build(String representation) {
-        return null;
+        //Error check for null strings
+        if (representation == null){
+            throw new NullPointerException("Variable representation cannot be null");
+        }
+        else if (false){
+            //TEST TO SEE IF VARIABLE ALREADY EXISTS
+        }
+        else {
+            return new Variable(representation);
+        }
+
     }
 }
